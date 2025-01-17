35e61a5b0df319f7b6f6f1b71ead70d8967f0ffd
Jean Nunes <jean.nunes@uzzipay.com>
2025-01-17 13:27:54 -0400

    Set Vitest

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,12 @@
+import swc from "unplugin-swc";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    include: ["**/*.e2e-spec.ts"],
+    globals: true,
+    root: "./",
+  },
+  plugins: [tsConfigPaths(), swc.vite()],
+});
