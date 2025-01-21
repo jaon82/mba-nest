acbb16ba5e8f31cecc7fdc7bb9a82b7c3347e149
Jean Nunes <jean.nunes@uzzipay.com>
2025-01-21 14:04:05 -0400

    Create Mapper

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -2,11 +2,23 @@ import { PaginationParams } from "@/core/repositories/pagination-params";
 import { IQuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 import { Injectable } from "@nestjs/common";
+import { PrismaQuestionMapper } from "../mappers/prisma-question-mapper";
+import { PrismaService } from "../prisma.service";
 
 @Injectable()
 export class PrismaQuestionsRepository implements IQuestionsRepository {
-  findById(id: string): Promise<Question | null> {
-    throw new Error("Method not implemented.");
+  constructor(private prismaService: PrismaService) {}
+
+  async findById(id: string): Promise<Question | null> {
+    const question = await this.prismaService.question.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!question) {
+      return null;
+    }
+    return PrismaQuestionMapper.toDomain(question);
   }
   findBySlug(slug: string): Promise<Question | null> {
     throw new Error("Method not implemented.");
