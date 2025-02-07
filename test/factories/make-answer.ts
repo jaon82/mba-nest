af7aee0290bdcbb630bc7cf8958c21eaedeef04d
Jean Nunes <jean.nunes@uzzipay.com>
2025-02-07 12:43:10 -0400

    Refactor e2e tests

--- test/factories/make-answer.ts
+++ test/factories/make-answer.ts
@@ -1,6 +1,9 @@
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Answer, AnswerProps } from "@/domain/forum/enterprise/entities/answer";
+import { PrismaAnswerMapper } from "@/infra/database/prisma/mappers/prisma-answer-mapper";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
 import { faker } from "@faker-js/faker";
+import { Injectable } from "@nestjs/common";
 
 export function makeAnswer(
   override: Partial<AnswerProps> = {},
@@ -16,4 +19,16 @@ export function makeAnswer(
     id
   );
   return answer;
+}
+
+@Injectable()
+export class AnswerFactory {
+  constructor(private prismaService: PrismaService) {}
+  async makePrismaAnswer(data: Partial<AnswerProps> = {}): Promise<Answer> {
+    const answer = makeAnswer(data);
+    await this.prismaService.answer.create({
+      data: PrismaAnswerMapper.toPrisma(answer),
+    });
+    return answer;
+  }
 }
