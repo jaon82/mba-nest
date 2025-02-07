9869a7c09a349161cf8b9a5346b457d3f128cff0
Jean Nunes <jean.nunes@uzzipay.com>
2025-02-07 13:28:03 -0400

    Comment on question

--- src/domain/forum/application/use-cases/comment-on-question.ts
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -1,7 +1,8 @@
 import { Either, left, right } from "@/core/either";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
-import { IQuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository";
+import { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository";
 import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment";
+import { Injectable } from "@nestjs/common";
 import { ResourceNotFoundError } from "../../../../core/errors/resource-not-found-error";
 import { QuestionsRepository } from "../repositories/questions-repository";
 
@@ -18,10 +19,11 @@ type CommentOnQuestionUseCaseResponse = Either<
   }
 >;
 
+@Injectable()
 export class CommentOnQuestionUseCase {
   constructor(
     private questionsRepository: QuestionsRepository,
-    private questionCommentsRepository: IQuestionCommentsRepository
+    private questionCommentsRepository: QuestionCommentsRepository
   ) {}
 
   async execute({
