5e7a89ed3d5bf82b0890ae6a459bfdde1af4b362
Jean Nunes <jean.nunes@uzzipay.com>
2025-02-13 12:20:44 -0400

    Fetch answer comments with author

--- src/infra/database/prisma/repositories/prisma-answer-comments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answer-comments-repository.ts
@@ -1,13 +1,15 @@
 import { PaginationParams } from "@/core/repositories/pagination-params";
-import { IAnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
+import { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
 import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
+import { CommentWithAuthor } from "@/domain/forum/enterprise/entities/value-objects/comment-with-author";
 import { Injectable } from "@nestjs/common";
 import { PrismaAnswerCommentMapper } from "../mappers/prisma-answer-comment-mapper";
+import { PrismaCommentWithAuthorMapper } from "../mappers/prisma-comment-with-author-mapper";
 import { PrismaService } from "../prisma.service";
 
 @Injectable()
 export class PrismaAnswerCommentsRepository
-  implements IAnswerCommentsRepository
+  implements AnswerCommentsRepository
 {
   constructor(private prisma: PrismaService) {}
 
@@ -38,6 +40,26 @@ export class PrismaAnswerCommentsRepository
       skip: (page - 1) * 20,
     });
     return answerComments.map(PrismaAnswerCommentMapper.toDomain);
+  }
+
+  async findManyByAnswerIdWithAuthor(
+    answerId: string,
+    { page }: PaginationParams
+  ): Promise<CommentWithAuthor[]> {
+    const answerComments = await this.prisma.comment.findMany({
+      where: {
+        answerId,
+      },
+      include: {
+        author: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    });
+    return answerComments.map(PrismaCommentWithAuthorMapper.toDomain);
   }
 
   async create(answerComment: AnswerComment): Promise<void> {
--- src/infra/http/controllers/fetch-answer-comments.controller.ts
+++ src/infra/http/controllers/fetch-answer-comments.controller.ts
@@ -8,7 +8,7 @@ import {
   Query,
 } from "@nestjs/common";
 import { z } from "zod";
-import { CommentPresenter } from "../presenters/comment-presenter";
+import { CommentWithAuthorPresenter } from "../presenters/comment-with-author-presenter";
 
 const pageQueryParamSchema = z
   .string()
@@ -35,7 +35,7 @@ export class FetchAnswerCommentsController {
     if (result.isLeft()) {
       throw new BadRequestException();
     }
-    const answerComments = result.value.answerComments;
-    return { comments: answerComments.map(CommentPresenter.toHTTP) };
+    const comments = result.value.comments;
+    return { comments: comments.map(CommentWithAuthorPresenter.toHTTP) };
   }
 }
